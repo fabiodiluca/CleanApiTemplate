7105ce7c0779cc1a948a94b92bb688d63e4a70d0
Fabio Di Luca <fabio.diluca@gmail.com>
2021-05-29 12:50:14 -0300

    feat: Throws CommitException comment

--- src/UnitOfWork/IUnitOfWork.cs
+++ src/UnitOfWork/IUnitOfWork.cs
@@ -6,7 +6,15 @@ namespace CleanTemplate.UnitOfWork
     {
         object Session { get; }
         void BeginTransaction();
+        /// <summary>
+        /// Throws CommitException
+        /// </summary>
         void Commit();
+        /// <summary>
+        /// Throws CommitException
+        /// </summary>
+        /// <param name="startNewSession"></param>
+        /// <param name="startNewTransaction"></param>
         void Commit(bool startNewSession, bool startNewTransaction);
         void RollbackTransaction();
         void NewSession();
